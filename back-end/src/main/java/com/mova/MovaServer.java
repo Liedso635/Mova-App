127e5741c9e1c129a02d4dd5e992cc049c3d027c
SM <sm@Sousas-MacBook-Air.local>
2026-06-07 19:56:33 +0200

    versao webapp

--- back-end/src/main/java/com/mova/MovaServer.java
+++ back-end/src/main/java/com/mova/MovaServer.java
@@ -12,19 +12,19 @@ public class MovaServer {
     }
 
     private static void enableCORS() {
-        options("/*", (request, response) -> {
-            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+        options("/*", (req, res) -> {
+            String accessControlRequestHeaders = req.headers("Access-Control-Request-Headers");
             if (accessControlRequestHeaders != null)
-                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
-            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+                res.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            String accessControlRequestMethod = req.headers("Access-Control-Request-Method");
             if (accessControlRequestMethod != null)
-                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+                res.header("Access-Control-Allow-Methods", accessControlRequestMethod);
             return "OK";
         });
-        before((request, response) -> {
-            response.header("Access-Control-Allow-Origin", "*");
-            response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-            response.header("Access-Control-Allow-Headers", "*");
+        before((req, res) -> {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            res.header("Access-Control-Allow-Headers", "*");
         });
     }
 }
---
+++ back-end/src/main/java/com/mova/model/Viagem.java
@@ -0,0 +1,28 @@
+package com.mova.model;
+
+import com.mova.structures.ListaDuplamenteLigada;
+
+public class Viagem implements Comparable<Viagem> {
+    private int id;
+    private String origemId;
+    private String destinoId;
+    private ListaDuplamenteLigada rotaIds;
+    private double distancia;
+
+    public Viagem(int id, String origemId, String destinoId) {
+        this.id = id; this.origemId = origemId; this.destinoId = destinoId;
+        this.rotaIds = new ListaDuplamenteLigada();
+    }
+    public int getId() { return id; }
+    public String getOrigemId() { return origemId; }
+    public String getDestinoId() { return destinoId; }
+    public ListaDuplamenteLigada getRotaIds() { return rotaIds; }
+    public void setRotaIds(ListaDuplamenteLigada rotaIds) { this.rotaIds = rotaIds; }
+    public double getDistancia() { return distancia; }
+    public void setDistancia(double distancia) { this.distancia = distancia; }
+
+    @Override
+    public int compareTo(Viagem o) {
+        return Integer.compare(this.id, o.id);
+    }
+}
